33a2bc939aeff52457af1730c0305a6cc5ca95e1
akashvicky1 <143933514+akashvicky1@users.noreply.github.com>
2026-05-08 15:14:11 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,18 +2,6 @@
 
     // 🔥 CURSOR GLOW
 
-const glow =
-  document.getElementById("cursorGlow");
-
-document.addEventListener("mousemove", (e) => {
-
-  glow.style.left =
-    e.clientX + "px";
-
-  glow.style.top =
-    e.clientY + "px";
-
-});
 
     tsParticles.load("tsparticles", {
       fullScreen: { enable: true },
